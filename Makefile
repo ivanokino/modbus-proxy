1af6a955bd95dd47182346954d8514a21cd7d603
ivanokino <ivanokfadratov@gmail.com>
2026-08-24 02:04:26 +0300

    Rewrite proxy.rs with tokio

--- Makefile
+++ Makefile
@@ -1,10 +1,12 @@
-
-
 all:
+	cargo build
+	cp target/debug/proxy ./proxy
+	cp target/debug/client ./client
+	gcc -o server src/server.c
 
-	rustc src/client.rs
-	rustc src/proxy.rs
-	gcc -o server.o src/server.c
+clean:
+	cargo clean
+	rm -f proxy client server
 
 
 
--- src/proxy.rs
+++ src/proxy.rs
@@ -1,54 +1,78 @@
 use std::{
-    convert::TryInto,
-    io::{Read, Write},
-    net::{TcpListener, TcpStream},
+ sync::Arc,
 };
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind("127.0.0.1:8080")?;
-    let mut write_to_host_stream = TcpStream::connect("127.0.0.1:8081")?;
+use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::{TcpListener, TcpStream}, sync::Mutex};
 
-    for stream in listener.incoming() {
-        let mut stream = stream?;
 
-        let buff = handle(&mut stream)?;
 
-        let is_normal = check(&buff);
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    let write_to_host_stream = TcpStream::connect("127.0.0.1:8081").await?;
 
-        if !is_normal {
-            stream.write_all("bad package".as_bytes())?;
-            stream.shutdown(std::net::Shutdown::Both)?;
-            println!("bad package");
-        } else {
-            stream.write_all("success".as_bytes())?;
-            write_to_host_stream.write_all(&buff)?;
-        }
+    let write_to_host_stream = Arc::new(Mutex::new(write_to_host_stream));
+
+    loop {
+        let (mut stream, _) = listener.accept().await?;
+        let host_clone = Arc::clone(&write_to_host_stream);
+        
+        tokio::spawn(async move{
+
+            let buff = match handle(&mut stream).await {
+                Ok(x) => x, 
+                Err(_) => return,
+            };
+
+            let is_normal = check(&buff);
+
+            if !is_normal {
+                _ = stream.write_all("bad packet".as_bytes()).await;
+                _ = stream.shutdown().await;
+                println!("bad packet");
+            } else {
+                
+                let mut guard = host_clone.lock().await;
+
+                let _ = guard.write_all(&buff).await;
+
+                let _ = stream.write("success".as_bytes()).await;
+            }
+    }    );
     }
 
-    Ok(())
+  
 }
 
-fn handle(stream: &mut TcpStream) -> Result<Vec<u8>, std::io::Error> {
+async fn handle(stream: &mut TcpStream) -> Result<Vec<u8>, std::io::Error> {
     let mut buff = [0; 1024];
 
-    let nofbytes = stream.read(&mut buff)?;
+    let nofbytes = stream.read(&mut buff).await?; // (number of bytes)
 
     Ok(buff[..nofbytes].to_vec())
 }
 
 fn check(buff: &[u8]) -> bool {
-    let bytes: [u8; 2] = buff[4..6].try_into().expect("error in the check()");
-    let lenght: u16 = u16::from_be_bytes(bytes);
 
-    if (lenght as usize) != (buff.len() - 6)
+
+
+    if buff.len() < 7 {
+        return  false;
+    }
+
+
+    let length: u16 = u16::from_be_bytes([buff[4], buff[5]]);
+
+
+    if (length as usize) != (buff.len() - 6)
         || (buff[2] != 0 && buff[3] != 0)
-        || buff.len() < 7
         || buff.len() > 260
     {
         return false;
     }
 
-    println!("lenght: {}", lenght);
+    println!("length: {}", length);
 
     true
 }
